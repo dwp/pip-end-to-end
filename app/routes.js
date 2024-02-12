432398f75505a22a8e4179685f8a63e2c6d3f535
ladine-cook <ladine.cook@engineering.digital.dwp.gov.uk>
2024-02-12 09:31:30 +0000

    Initial commit
    GitOrigin-RevId: 217618de351bfe673586fbeb3b8cac1a7acffd99

--- app/routes.js
+++ app/routes.js
@@ -3,22 +3,7 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const util = require('util')
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// ROUTES REDIRECT START
-
-const applyforpip = require('./routes/apply-for-pip')
-const pipregister = require('./routes/pip-register')
-const agent = require('./routes/agent')
-
-// ROUTES REDIRECT END
-
-// ROUTER LIST START
-
-applyforpip(router);
-pipregister(router);
-agent(router);
-
-// ROUTER LIST END
+// Add your routes here
